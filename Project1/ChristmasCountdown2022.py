bfbf9e0704e2edf761efb3a350d743c44e0bfe0d
daydayrm <75863960+daydayrm@users.noreply.github.com>
2022-10-09 15:46:01 -0400

    Update and rename FirstProject.txt to ChristmasCountdown2022.py
    Christmas Countdown 2022. This is project 1. It is a simple script that asks the user for their favorite Christmas movie. Once receiving the input, the script will display a countdown of how many days left "until Santa comes" in red and green font.

---
+++ Project1/ChristmasCountdown2022.py
@@ -0,0 +1,18 @@
+import datetime
+class colors:
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    RESET = '\033[0m'
+
+dt = datetime.datetime
+today = dt.now()
+
+daysUntil = dt(year = 2022, month = 12, day = 25) - dt(year = today.year, month = today.month, day = today.day)
+
+favMovie = input("Christmas 2022 is approaching! Enter your favorite Christmas Movie to see how many days until Christmas. Hit enter when you are finished. ")
+print ((favMovie) + " is a good one!! My favorite is The Elf! Now.. the countdown begins!")
+
+
+
+print (colors.RED + "CHRISTMAS" + colors.RESET + colors.GREEN + " COUNTDOWN" + colors.RESET + colors.RED + " 2" + colors.RESET + colors.GREEN + "0" + colors.RESET + colors.RED + "2" + colors.RESET + colors.GREEN + "2" + colors.RESET + colors.RED + "!" + colors.RESET)
+print ("There are " + colors.GREEN + str(daysUntil.days) + colors.RESET + " days until" + colors.RED + " Santa" + colors.RESET + " comes!")
