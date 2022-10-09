cf67d306af31e13abf316c988676c5e3c038e7ea
daydayrm <75863960+daydayrm@users.noreply.github.com>
2022-10-09 19:24:29 -0400

    Update and rename nodefolder.txt to test.js

---
+++ node/test.js
@@ -0,0 +1,7 @@
+var path = required("path");
+var hello = "Hello from Node JS Variable!"
+'console.log(hello)'
+
+console.log("Using PATH module:");
+console.log('Hello from file ${path.basename(__filename)}');
+console.log('Process args: ${process.argv}')
