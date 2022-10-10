9de802c24ebc8787fb60b2a53fd31f2778d8ea26
daydayrm <75863960+daydayrm@users.noreply.github.com>
2022-10-09 20:20:21 -0400

    Update server.js

--- node/server.js
+++ node/server.js
@@ -6,7 +6,7 @@ var ip = require('ip');
 http.createServer(function(req, res){
 
     if (req.url === "/") {
-        fs.readFile("./public/index.html", "UTF-8", function(err, body){
+        fs.readFile("./Public/index.html", "UTF-8", function(err, body){
         res.writeHead(200, {"Content-Type": "text/html"});
         res.end(body);
     });
